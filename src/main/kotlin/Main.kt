c04726131cb47a54fb1b650d29ad1ca3649ce72f
jhenriquedsilva <josehs555@gmail.com>
2022-06-20 21:10:45 -0300

    feat: roda código na main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import componentes_grafo.TipoAresta
+import dijkstra.Dijkstra
 import grafo_com_lista_adjacencia.ListaAdjacencia
 
 fun main() {
@@ -51,6 +52,27 @@ fun main() {
 //    graph.adicionar(TipoAresta.NAO_DIRECIONADO, austinTexas, sanFrancisco, 297.0)
 //    println(graph)
 
+//
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, b, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, c, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, d, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, b, e, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, c, f, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, c, g, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, e, h, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, e, f, null)
+//    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, f, g, null)
+//
+////    val vertices = graph.buscaEmLargura(a)
+////    vertices.forEach { vertice ->
+////        println(vertice.dado)
+////    }
+//
+//    val vertices = grafo.buscaEmProfundidade(a)
+//    vertices.forEach { vertice ->
+//        println(vertice.dado)
+//    }
+
     val grafo = ListaAdjacencia<String>()
     val a = grafo.criarVertice("A")
     val b = grafo.criarVertice("B")
@@ -61,23 +83,25 @@ fun main() {
     val g = grafo.criarVertice("G")
     val h = grafo.criarVertice("H")
 
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, b, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, c, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, a, d, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, b, e, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, c, f, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, c, g, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, e, h, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, e, f, null)
-    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, f, g, null)
+    grafo.adicionar(TipoAresta.DIRECIONADO, a, b, 8.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, a, f, 9.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, a, g, 1.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, b, f, 3.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, b, e, 1.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, f, a, 2.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, h, f, 2.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, h, g, 5.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, g, c, 3.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, c, e, 1.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, c, b, 3.0)
+    grafo.adicionar(TipoAresta.NAO_DIRECIONADO, e, c, 8.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, e, b, 1.0)
+    grafo.adicionar(TipoAresta.DIRECIONADO, e, d, 2.0)
 
-//    val vertices = graph.buscaEmLargura(a)
-//    vertices.forEach { vertice ->
-//        println(vertice.dado)
-//    }
-
-    val vertices = grafo.buscaEmProfundidade(a)
-    vertices.forEach { vertice ->
-        println(vertice.dado)
+    val dijkstra = Dijkstra(grafo)
+    val caminhosIniciandoNoVerticeA = dijkstra.calcularCaminhoMaisCurto(g)
+    val caminho = dijkstra.calcularCaminhoMaisCurto(a, caminhosIniciandoNoVerticeA)
+    caminho.reversed().forEach { aresta ->
+        println("${aresta.origem.dado} --|${aresta.peso ?: 0.0}|--> ${aresta.destino.dado}")
     }
 }
